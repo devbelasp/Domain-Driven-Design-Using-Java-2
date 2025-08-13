054bc1e39d6227698c3d7c815a4e64f748a230d4
devbelasp <isa.bela360@hotmail.com>
2025-08-13 13:36:29 -0300

    Correção do exercício - HashMap

--- Projeto03/src/br/com/fiap/main/PokemonHashMap.java
+++ Projeto03/src/br/com/fiap/main/PokemonHashMap.java
@@ -3,37 +3,43 @@ package br.com.fiap.main;
 import javax.swing.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class PokemonHashMap {
     public static void main(String[] args) {
-        HashMap<String, String> mapa = new HashMap<String, String>();
+        HashMap<String, String> pokemons = new HashMap<>();
+
         do {
             try {
-                String nomePokemon, tipoPokemon;
+                String nome, tipo;
                 do {
-                    nomePokemon = JOptionPane.showInputDialog("Digite o nome do Pokemon ou digite \"FIM\" para encerrar.").toUpperCase();
+                    nome = JOptionPane.showInputDialog("Digite o nome de um pokémon ou digite \"FIM\" para encerrar").toUpperCase();
 
-                    if (!nomePokemon.equals("FIM")) {
-                        if (mapa.containsKey(nomePokemon)) {
-                            JOptionPane.showMessageDialog(null, "Este Pokemon já foi cadastrado!");
+                    if (!nome.equals("FIM")) {
+                        if (pokemons.containsKey(nome)) {
+                            JOptionPane.showMessageDialog(null, "Este pokémon já foi cadastrado!");
                         } else {
-                            tipoPokemon = JOptionPane.showInputDialog("Digite o tipo do Pokemon informado anteriormente.");
-                            mapa.put(nomePokemon, tipoPokemon);
+                            tipo = JOptionPane.showInputDialog("Digite o tipo do pokémon informado anteriormente").toUpperCase();
+                            pokemons.put(nome, tipo);
                         }
                     }
-                } while (!nomePokemon.equals("FIM"));
 
-                String escolha = JOptionPane.showInputDialog("Digite o tipo do Pokemon de sua escolha.").toUpperCase();
-                if (mapa.containsKey(escolha)) {
-                    JOptionPane.showMessageDialog(null, "Os Pokemons cadastrados deste tipo são:" + mapa.get(escolha), "Pokemons", JOptionPane.INFORMATION_MESSAGE);
-                } else {
-                    JOptionPane.showMessageDialog(null, "Pokemon não cadastrado!", "Atenção", JOptionPane.WARNING_MESSAGE);
+                } while (!nome.equals("FIM"));
+
+                String escolha = JOptionPane.showInputDialog("Digite um TIPO a sua escolha").toUpperCase();
+                ArrayList<String> nomesEncontrados = new ArrayList<>();
+                for (Map.Entry<String, String> pokemon : pokemons.entrySet()) {
+                    if (pokemon.getValue().equals(escolha)) {
+                        nomesEncontrados.add(pokemon.getKey());
+                    }
                 }
+                JOptionPane.showMessageDialog(null, String.format("Para o tipo: %s foram encontrado(s) o(s) pokémon(s)\n%s", escolha, nomesEncontrados));
+
             } catch (Exception e) {
                 JOptionPane.showMessageDialog(null, e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
             }
+
         } while (JOptionPane.showConfirmDialog(null, "Deseja continuar?", "Atenção", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == 0);
         JOptionPane.showMessageDialog(null, "Fim de Programa!", "Adeus", JOptionPane.WARNING_MESSAGE);
     }
 }
-
