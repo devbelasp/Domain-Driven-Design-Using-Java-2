1d54c5bed4fed1646919cb797eae90bb72f90d4a
devbelasp <isa.bela360@hotmail.com>
2025-10-29 01:05:19 -0300

    Implementação do Módulo Cliente DAO para Projeto Estacionamento

---
+++ A10 - JDBC e Design Patterns DAO/src/br/com/fiap/main/TesteClienteCRUD.java
@@ -0,0 +1,52 @@
+package br.com.fiap.main;
+
+import br.com.fiap.dao.ClienteDAO;
+import br.com.fiap.dao.ConnectionFactory;
+import br.com.fiap.dto.Cliente;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+public class TesteClienteCRUD {
+    public static void main(String[] args) {
+        Connection con = ConnectionFactory.abrirConexao();
+        // Inserindo 4 clientes novos
+        Cliente cliente = new Cliente();
+        ClienteDAO clienteDAO = new ClienteDAO(con);
+        cliente.setIdCliente(1);
+        cliente.setNomeCliente("Astrogildo");
+        cliente.setPlaca("JKK1900");
+        System.out.println(clienteDAO.inserir(cliente));
+        cliente.setIdCliente(2);
+        cliente.setNomeCliente("Berisvaldo");
+        cliente.setPlaca("JKK1901");
+        System.out.println(clienteDAO.inserir(cliente));
+        cliente.setIdCliente(3);
+        cliente.setNomeCliente("Gumercindo");
+        cliente.setPlaca("JKK1902");
+        System.out.println(clienteDAO.inserir(cliente));
+        cliente.setIdCliente(4);
+        cliente.setNomeCliente("Pafúncia");
+        cliente.setPlaca("JKK1903");
+        System.out.println(clienteDAO.inserir(cliente));
+        // Alterando os dados de um cliente
+        cliente.setIdCliente(3);
+        cliente.setNomeCliente("Josenelson");
+        cliente.setPlaca("JKK1903");
+        System.out.println(clienteDAO.alterar(cliente));
+        // Excluindo um cliente
+        cliente.setIdCliente(2);
+        System.out.println(clienteDAO.excluir(cliente));
+        // Listando todos os clientes
+        ArrayList<Cliente> resultado = clienteDAO.listarTodos();
+        if (resultado != null) {
+            for(Cliente cliente1 : resultado) {
+                System.out.println("Id: " + cliente1.getIdCliente());
+                System.out.println("Nome: " + cliente1.getNomeCliente());
+                System.out.println("Placa: " + cliente1.getPlaca());
+                System.out.println();
+            }
+        }
+        ConnectionFactory.fecharConexao(con);
+    }
+}
